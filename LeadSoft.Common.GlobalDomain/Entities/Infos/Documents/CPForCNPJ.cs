134292d811ae4e8fd2ec14284a7abd317959da81
Lucas Tavares <eu@lucasrtavares.com.br>
2026-07-14 17:57:12 -0300

    New CPF or CNPJ attribute method

---
+++ LeadSoft.Common.GlobalDomain/Entities/Infos/Documents/CPForCNPJ.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeadSoft.Common.GlobalDomain.Entities.Infos.Documents
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+    public class CPForCNPJAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            if (value is null)
+                return true;
+
+            string str = value.ToString()!;
+
+            if (str.IsCpf() || str.IsCnpj())
+                return true;
+
+            ErrorMessage ??= "CPF ou CNPJ inválido.";
+            return false;
+        }
+    }
+}
